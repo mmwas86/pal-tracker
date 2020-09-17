e13ff1ec058fe285d3cf2c0dc70a776c35d661de
Pat <umdwas88@gmail.com>
2020-09-17 17:07:41 +0000

    lab 6 jdbc

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,13 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    /*
     @Bean
     public TimeEntryRepository inMeoryDAO()
     { return new InMemoryTimeEntryRepository();
     }
+     */
+
+    @Bean
+    public JdbcTimeEntryRepository jdbcTimeEntry(DataSource dataSource)
+    {
+        //Spring smart enough to wire datasource?
+        return new JdbcTimeEntryRepository(dataSource);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
